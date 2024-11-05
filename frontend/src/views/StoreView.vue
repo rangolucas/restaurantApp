038abdccb61f9c85f129f85f37112ec21a1eab07
Lucas Rango <lucasrango@gmail.com>
2024-11-05 01:40:07 +0000

    StoreView draft

---
+++ frontend/src/views/StoreView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+</script>
+
+<template>
+  <div>
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Pedidos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Menu</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
