a79c50090be91ff90640ac91d25a90aa16f53586
Lucas Rango <lucasrango@gmail.com>
2024-11-16 20:57:59 +0000

    Update get stores response

--- backend/infrastructure/controller/StoreController.rb
+++ backend/infrastructure/controller/StoreController.rb
@@ -8,15 +8,13 @@ class StoreController
   end
 
   def create_store(request)
-    coordinates = request['coordinates']
-    time_table = request['timeTable']
-    contact_info = request['contactInfo']
-    store_id = @create_store_action.invoke(coordinates, time_table, contact_info)
+    store_id = @create_store_action.invoke(request['coordinates'], request['timeTable'], request['contactInfo'])
     encode_store_id_to_json(store_id)
   end
 
   def get_stores
-    @get_stores_action.invoke().map { |s| encode_store_to_json(s) }.to_json
+    all_stores = @get_stores_action.invoke
+    encode_stores_to_json(all_stores)
   end
 
   def get_menu(store_id)
@@ -28,8 +26,13 @@ class StoreController
   def encode_store_to_json(store)
     {
       "id": store.id,
-      "contact": store.contact_info
+      "contact": store.contact_info,
+      "coordinates": store.coordinates
     }
+  end
+
+  def encode_stores_to_json(stores)
+    stores.map { |s| encode_store_to_json(s) }.to_json
   end
 
   def encode_store_id_to_json(id)
