6ced347dc77f1bc7428972216cb9299c40312f4e
Martín <martindaujotas@gmail.com>
2024-11-16 20:35:52 +0000

    add basic order view

--- frontend/src/views/OrdersView.vue
+++ frontend/src/views/OrdersView.vue
@@ -1,7 +1,25 @@
 <script setup>
+import { ref } from 'vue';
+
+const orderStatus = ref('En preparación');
+const orderId = ref('123456');
 </script>
+
 <template>
   <main>
-    <h1>Orden</h1>
+    <h1>Número de la orden: #{{ orderId }}</h1>
+    <h2>Estado: {{ orderStatus }}</h2>
+
   </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  padding: 20px;
+}
+
+</style>
