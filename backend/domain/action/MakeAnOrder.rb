7d389b5dd24d6f67a72aa5d1d6be6df07cab871a
Lucas Rango <lucasrango@gmail.com>
2024-11-17 01:12:21 +0000

    Improve single level of abstractoin in MakeAnOrder.rb

--- backend/domain/action/MakeAnOrder.rb
+++ backend/domain/action/MakeAnOrder.rb
@@ -5,17 +5,23 @@ class MakeAnOrder
     @store_repository = store_repository
   end
 
-  # Returns true only if the user is 50 meters within the store radius
+  # Returns true only if the user is close enough to the store
   def invoke(store_id, user_id, amount_by_item, user_coordinates)
     store = @store_repository.fetch_by_id(store_id)
     if store.is_close_to(user_coordinates)
-      @store_repository.update_store(
-        store_id,
-        -> (saved_store) { saved_store.register_order(user_id, amount_by_item) }
-      )
+      register_order(store_id, user_id, amount_by_item)
       true
     else
       false
     end
+  end
+
+  private
+
+  def register_order(store_id, user_id, amount_by_item)
+    @store_repository.update_store(
+        store_id,
+        -> (saved_store) { saved_store.register_order(user_id, amount_by_item) }
+      )
   end
 end
