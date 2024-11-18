e443beec984397aff3a6b0039677f4018553d6a9
Martín <martindaujotas@gmail.com>
2024-11-18 20:38:48 +0000

    fix map caused by getStores() delay

--- frontend/src/components/StoresMap.vue
+++ frontend/src/components/StoresMap.vue
@@ -26,6 +26,57 @@ export default {
       required: true,
     },
   },
+  methods: {
+    initializeMapAndMarkers(center, stores) {
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: center,
+        zoom: 12,
+        mapId: "a68d2e12be21b6a3",
+        mapTypeControl: false,
+        streetViewControl: false,
+      });
+
+      // Add markers for each store
+      stores.forEach((store) => {
+        const distance = calculateDistance(center.lat, center.lng, store.lat, store.long);
+        const pinForFarStores = new google.maps.marker.PinElement({
+          background: "grey",
+          glyphColor: "white",
+          borderColor: "grey"
+        });
+        const pinForCloseStores = new google.maps.marker.PinElement({
+          background: "red",
+          glyphColor: "white",
+          borderColor: "red"
+        });
+        const marker = new google.maps.marker.AdvancedMarkerElement({
+          map,
+          position: { lat: store.lat, lng: store.long },
+          content: distance > DISTANCE_THRESHOLD ? pinForFarStores.element : pinForCloseStores.element,
+        });
+
+        // Add click event listener to the marker
+        marker.addListener('click', () => {
+          const infoWindow = new google.maps.InfoWindow({
+            content: distance > DISTANCE_THRESHOLD ?
+              `<div>
+                <h6 style="color:grey;"><strong>${store.name}</strong></h6>
+                ${store.address}
+                <p>Horarios: ${store.hours}</p>
+                <p>Distancia: ${distance.toFixed(2)} km</p>
+              </div>` :
+              `<div>
+                <h6><strong><a href="/user/${store.id}">${store.name}</a></strong></h6>
+                <br>${store.address}
+                <br><p>Horarios: ${store.hours}</p>
+                <p>Distancia: ${distance.toFixed(2)} km</p>
+              </div>`
+          });
+          infoWindow.open(map, marker);
+        });
+      });
+    }
+  },
   mounted() {
     const loader = new Loader({
       apiKey: "AIzaSyDgRY5NQGY3JwSGdpM8HMzLKBuZc9OqI2E",
@@ -34,75 +85,51 @@ export default {
     });
 
     loader.load().then(() => {
-      const initializeMap = (center) => {
-        const map = new google.maps.Map(document.getElementById("map"), {
-          center: center,
-          zoom: 12,
-          mapId: "a68d2e12be21b6a3",
-          mapTypeControl: false,
-          streetViewControl: false,
-        });
-
-        // Add markers for each store
-        this.stores.forEach((store) => {
-          const distance = calculateDistance(center.lat, center.lng, store.lat, store.long);
-          const pinForFarStores = new google.maps.marker.PinElement({
-                background: "grey",
-                glyphColor: "white",
-                borderColor: "grey"
-          });
-          const pinForCloseStores = new google.maps.marker.PinElement({
-                background: "red",
-                glyphColor: "white",
-                borderColor: "red"
-          });
-          const marker = new google.maps.marker.AdvancedMarkerElement({
-            map,
-            position: { lat: store.lat, lng: store.long },
-            content: distance > DISTANCE_THRESHOLD ? pinForFarStores.element : pinForCloseStores.element,
-          });
-
-          // Add click event listener to the marker
-          marker.addListener('click', () => {
-            const infoWindow = new google.maps.InfoWindow({
-              content: distance > DISTANCE_THRESHOLD ?
-
-                `<div>
-                <h6 style="color:grey;"><strong>${store.name}</strong></h6>
-                ${store.address}
-                <p>Horarios: ${store.hours}</p>
-                <p>Distancia: ${distance.toFixed(2)} km</p>
-                </div>` :
-
-                `<div>
-                <h6><strong><a href="/user/${store.id}">${store.name}</a></strong></h6>
-                <br>${store.address}
-                <br><p>Horarios: ${store.hours}</p>
-                <p>Distancia: ${distance.toFixed(2)} km</p>
-                </div>`
-            });
-              infoWindow.open(map, marker);
-          });
-        });
-      };
-
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
           (position) => {
             const { latitude, longitude } = position.coords;
-            this.updateLocation({lat : latitude, lng : longitude});
-            initializeMap({ lat: latitude, lng: longitude });
+            this.updateLocation({ lat: latitude, lng: longitude });
+            this.initializeMapAndMarkers({ lat: latitude, lng: longitude }, this.stores);
           },
           (error) => {
             console.error("Error getting current location:", error);
-            initializeMap(DEFAULT_COORDINATES); // Default to CABA
+            this.initializeMapAndMarkers(DEFAULT_COORDINATES, this.stores); // Default to CABA
           }
         );
       } else {
         console.error("Geolocation is not supported by this browser.");
-        initializeMap(DEFAULT_COORDINATES); // Default to CABA
+        this.initializeMapAndMarkers(DEFAULT_COORDINATES, this.stores); // Default to CABA
       }
     });
+  },
+  watch: {
+    stores(newStores) {
+      const loader = new Loader({
+        apiKey: "AIzaSyDgRY5NQGY3JwSGdpM8HMzLKBuZc9OqI2E",
+        version: "weekly",
+        libraries: ["places", "marker"],
+      });
+
+      loader.load().then(() => {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              const { latitude, longitude } = position.coords;
+              this.updateLocation({ lat: latitude, lng: longitude });
+              this.initializeMapAndMarkers({ lat: latitude, lng: longitude }, newStores);
+            },
+            (error) => {
+              console.error("Error getting current location:", error);
+              this.initializeMapAndMarkers(DEFAULT_COORDINATES, newStores); // Default to CABA
+            }
+          );
+        } else {
+          console.error("Geolocation is not supported by this browser.");
+          this.initializeMapAndMarkers(DEFAULT_COORDINATES, newStores); // Default to CABA
+        }
+      });
+    },
   },
 };
 </script>
