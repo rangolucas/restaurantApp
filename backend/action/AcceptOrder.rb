499b74343471e5a53331c70cfee03536c0a31920
Lucas Rango <lucasrango@gmail.com>
2024-11-20 23:46:17 +0000

    Delete old comment

--- backend/action/AcceptOrder.rb
+++ backend/action/AcceptOrder.rb
@@ -3,7 +3,6 @@ class AcceptOrder
     @store_repository = store_repository
   end
 
-  # Returns true only if the order was in pending state
   def invoke(store_id, table_id)
     @store_repository.update_store(
       store_id,
